98cd7169442cf5f175387cca57a7cc23b420a503
CanerYesiltas <caneryesiltas1@gmail.com>
2025-01-28 19:55:57 -0500

    Add Login component with basic layout and styling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React from 'react';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className='loginDiv'>
+      <div className='h-[31.25rem] w-11/12  sm:w-[29.6875rem] bg-white rounded-[1.25rem] o-5 flex flex-col justify-center text-center '>
+      
+      <div className='flex justify-center items-center mt-2 gap-2'  >
+        <span className='w-[6px] h-[30px] bg-yellow-500' ></span>
+        <h1 className='text-[1.375rem] sm:text-[32px] font-montserrat font-[700] uppercase' >SKY SHOP</h1>
+      </div>
+      
+      <div className='my-2'>
+        <h3 className='font-montserrat font-[600]'  >SIGN IN</h3>
+        <p className='font-montserrat text-labelColor text-label mt-1'   >Enter your credentials to acces your account</p>
+      </div>
+      
+      </div>
+    </div>
+  );
 };
 
-export default Login;
+export default Login; 
