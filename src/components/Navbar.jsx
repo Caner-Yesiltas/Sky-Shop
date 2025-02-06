6928c963518a28709938eab8be13b2b877eac76a
CanerYesiltas <caneryesiltas1@gmail.com>
2025-02-06 02:58:35 -0500

    update navbar paths

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import logo from '../assets/logo3.png';
 import { closeNavbar, openNavbar } from '../helper/icons';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 const navigation = [
   {
@@ -19,28 +19,31 @@ const navigation = [
 
 const Navbar = () => {
   const [show,setShow] = useState(false);
+  const location = useLocation();
+
 
   return (
     <nav className='bg-navbarColor md:text-sm'>
-      <div className='gap-x-14 items-center max-w-screen-xl mx-auto px-4 md:flex md:px-8'>
-        <div  className='flex items-center justify-between py-5 ' >
-          <a href='/' target='true' className='flex items-center'>
+      <div className='gap-x-14 items-between max-w-screen-xl mx-auto  px-4 md:flex md:px-8'>
+        <div  className='flex items-center justify-center py-5 ' >
+          <a href='/' target='' className='flex items-center'>
            
-            <img src={logo} alt='logo' width={85} height={85} />
+            <img src={logo} alt='logo' width={75} height={75} />
             <span>Sky Shop</span>
           </a>
-          <div className='md:hidden'>
+          <div className='md:hidden md:flex'>
             <button  onClick={()=>setShow(!show)} className='text-gray hover:text-gray-800'>
               {show ? closeNavbar : openNavbar}
             </button>
           </div>
         </div>
-        <div className={`${show ? "flex flex-col" : "hidden"} md:flex-row flex-1 items-center`} >
+        <div className={`${show ? "flex flex-col" : "hidden"} md:flex md:flex-row flex-1 items-center`} >
+          
           <ul className='md:flex md:space-x-6' >
             {
               navigation.map((item)=>(
                 <li className='text-gray700 font-medium flex justify-center' key={item.title}  >
-                  <NavLink className={`block hover:bg-main rounded-full py-2 px-4 hover:text-white`} to={item.path}>{item.title}</NavLink>
+                  <NavLink className={`block hover:bg-main rounded-full py-2 px-4 hover:text-white  ${location.pathname === item.pathname ? "underline scale-110" : ""}    `} to={item.path}>{item.title}</NavLink>
                 </li>
               ))
             }
