4b79658f0af70a0fc525a9fce13400acee86893b
CanerYesiltas <caneryesiltas1@gmail.com>
2025-02-16 19:26:34 -0500

    feat:add api request with async ~ axios

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import Axios  from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Products = () => {
-  return (
-    <div>Products</div>
-  )
-}
+  const [products, setProducts] = useState([]); // If set to a string or null, accessing array methods like map() would cause an error.
+  const [search, setSearch] = useState("")
 
-export default Products
+  const getData = async () => {
+    try {
+      const {data} = await Axios.get(`https://dummyjson.com/products/search?q=${search}`)
+      // console.log(res.data.products);
+      setProducts(data.products);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
+    useEffect(() => {
+      
+    getData()
+ 
+    }, [])
+    
+
+  return <div className='mx-auto max' >
+
+
+  </div>;
+};
+
+export default Products;
